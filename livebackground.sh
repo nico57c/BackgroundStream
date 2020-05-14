0363c84b05fda5e0b81cf0b2965b3772e6a5f4ae
nico57c <>
2020-05-13 22:55:23 -0400

    Dual screen with xwinwrap rectangle shape

--- livebackground.sh
+++ livebackground.sh
@@ -8,4 +8,10 @@ sleep 4
 
 echo $$ > $BG_STREAM_PID
 
-exec xwinwrap -fs -fdt -ni -b -nf -- $LIVE_STREAM $LIVE_STREAM_ID WID
+if [ "$BG_STREAM_SHAPE" != "" ]; then
+
+	exec xwinwrap -s  -un  -ni -b -nf -sh $BG_STREAM_SHAPE -g $BG_STREAM_SIZE -- $LIVE_STREAM $LIVE_STREAM_ID WID
+else
+
+	exec xwinwrap -fs -fdt -ni -b -nf -- $LIVE_STREAM $LIVE_STREAM_ID WID
+fi
--- livevars.sh
+++ livevars.sh
@@ -5,10 +5,17 @@ export BG_HOME="/home/desktop/Applications/BackgroundStream"
 export BG_STREAM="$BG_HOME/livebackground.sh"
 export BG_STREAM_PID="$BG_HOME/livebackground.pid"
 
+# Default setup for fullsreen mode
+export BG_STREAM_SHAPE=""
+export BG_STREAM_SIZE=""
+
+# 4K monitor in dual screen mode
+#export BG_STREAM_SHAPE="rectangle"
+#export BG_STREAM_SIZE="3840x2160+0+0"
+
 export LIVE_STREAM="$BG_HOME/livestream.sh"
 
 export LIVE_STREAM_ID_FILE="$BG_HOME/livestream.id"
 export LIVE_STREAM_ID=`cat $LIVE_STREAM_ID_FILE`
 
 export LIVE_STREAM_PID="$BG_HOME/livestream.pid"
-
